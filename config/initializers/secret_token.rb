0cb458e9c4ae2ff12490bd19e351d2120a4aa34b
Prashant <prashant@blendit.com.my>
2012-07-01 09:24:14 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uradv2::Application.config.secret_token = '88579bec2f0f9551a8ffd5e51362830df2b2e3bf342f81cab8dca0a3f47337c9d1bccfb37c9cb54c00851e4d0d57dff74e0633593258b280a71e4a14714181eb'
