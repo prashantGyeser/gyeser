171e4f56f663443c7458cc010493243bd16dabc3
Prashant <prashant@blendit.com.my>
2012-07-04 17:02:43 +0530

    Added validation to check if the restaurant name already is entered in the database.  If it does the restaurant will not be created and an error will be shown

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ActiveRecord::Base
   attr_accessible :address, :detailsImageURL, :imageURL, :name, :tags, :deliveryTime
 	validates :name, :address, :tags, :deliveryTime, :presence => true
+	validates :name, :uniqueness => true
   has_many :menu_items
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
