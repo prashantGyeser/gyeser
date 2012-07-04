b28be10debb385982655a958790849e18fcd2ae0
Prashant <prashant@blendit.com.my>
2012-07-04 16:53:22 +0530

    Added validation for the restaurant creation page. Now the name, address, delivery time and tags have to be entered before the restaurant details are saved

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ActiveRecord::Base
   attr_accessible :address, :detailsImageURL, :imageURL, :name, :tags, :deliveryTime
+	validates :name, :address, :tags, :deliveryTime, :presence => true
   has_many :menu_items
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
